7cc17c0a98185df27ff6bc9b23e8db1055fd2afe
Alexander Nikiforov <nikiforov.java@gmail.com>
2024-07-26 16:32:48 +0400

    The third commit

--- src/com/alexnikiforov/java/git/Class2.java
+++ src/com/alexnikiforov/java/git/Class2.java
@@ -3,5 +3,6 @@ package com.alexnikiforov.java.git;
 public class Class2 {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Second line");
     }
 }
